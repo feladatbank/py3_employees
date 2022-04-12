294cb00c5deec13c44aa1879c1f4e5bc74ae20c8
Lóczy Levente <71691777+loczylevi@users.noreply.github.com>
2022-04-12 07:09:56 +0000

    4.feladat: keresés adatgyüjtés

--- main.py
+++ main.py
@@ -70,3 +70,14 @@ atlag = ossz / len(jovedelmek)
 print(f"3.3: az alkalmazottak havi átlag jövedelme: ${atlag:.1f}")
 
 #4
+
+bekeres = input("3.4: írd be a kerestt nevet:")
+kereso = [sor for sor in lista if bekeres == sor.nev]
+
+if len(kereso) > 0:
+  for sor in kereso:
+    print(f"    életkor:    {2022-sor.szuletes}")
+    print(f"    székhely:    {sor.helyileg_hol}")
+    print(f"    havi fizetés:    {(float(sor.jovedelem) * 361.51):.0f} HUF")
+else:
+  print("nincs ilyen nevű alkalmazott a cégnél")
